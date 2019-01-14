6d9b53b745d1d8f5b93585bb697e4f900cb3b9bb
Johnny Henry Vasco Calle <n1m0d0>
2019-01-14 19:56:35 -0400

    formulario de evento

--- app/src/main/java/com/example/john_pc/prueba/form_event.java
+++ app/src/main/java/com/example/john_pc/prueba/form_event.java
@@ -1,13 +1,104 @@
 package com.example.john_pc.prueba;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class form_event extends AppCompatActivity {
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class form_event extends AppCompatActivity implements Response.Listener<JSONArray>, Response.ErrorListener {
+
+    String auth;
+    String userName;
+    ProgressDialog mProgressDialog;
+    RequestQueue mRequestQueue;
+    JsonArrayRequest mJsonArrayRequest;
+    String url = "https://test.portcolon2000.site/api/parForm";
+    Intent ir;
+    Toast msj;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_form_event);
+
+        Bundle parametros = this.getIntent().getExtras();
+        auth = parametros.getString("auth");
+        userName = parametros.getString("userName");
+
+    }
+
+    private void cargarFormulario(){
+
+        mProgressDialog =  new ProgressDialog(this);
+        mProgressDialog.setMessage("Cargando...");
+        mProgressDialog.show();
+
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        mJsonArrayRequest = new JsonArrayRequest(Request.Method.GET,url,null, this, this){
+
+            @Override
+            public Map getHeaders() throws AuthFailureError {
+                HashMap headers = new HashMap();
+                headers.put("Authorization", auth); //authentication
+                return headers;
+            }
+
+        };
+
+        mRequestQueue.add(mJsonArrayRequest);
+
+    }
+
+    @Override
+    public void onResponse(JSONArray response) {
+
+        /*msj = Toast.makeText(this, "" + response, Toast.LENGTH_LONG);
+        msj.show();*/
+        mProgressDialog.hide();
+
+        try{
+
+            for(int i=0;i<response.length();i++) {
+
+                JSONObject form = response.getJSONObject(i);
+
+
+            }
+
+        }catch (JSONException e) {
+
+            e.printStackTrace();
+            msj = Toast.makeText(this, "" + e, Toast.LENGTH_LONG);
+            msj.show();
+
+        }
+
+    }
+
+    @Override
+    public void onErrorResponse(VolleyError error) {
+
+        mProgressDialog.hide();
+        msj = Toast.makeText(this, "Ocurrio un Error: " + error, Toast.LENGTH_LONG);
+        msj.show();
+
     }
 }
